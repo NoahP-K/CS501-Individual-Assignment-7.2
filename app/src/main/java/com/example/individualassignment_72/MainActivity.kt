d327b6fcc245d1a8384d1f5bd915630280216e79
NoahP-K <noahpk@bu.edu>
2025-03-24 21:22:02 -0400

    Added some comments.

--- app/src/main/java/com/example/individualassignment_72/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_72/MainActivity.kt
@@ -52,33 +52,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//function to handle checking the state of the viewmodel
 @Composable
 fun MakeSearch(searchState: AccountViewModel.SearchState,
-               //currentResults: SnapshotStateList<Repo>,
                viewModel: AccountViewModel,
                onSuccess: ()->Unit,
                onEndOfResults: ()->Unit,
                perPage: Int){
     when (searchState) {
-        AccountViewModel.SearchState.Initial -> {}
-        AccountViewModel.SearchState.Loading -> {
+        AccountViewModel.SearchState.Initial -> {}  //show nothing if in the initial state
+        AccountViewModel.SearchState.Loading -> {   //show loading icon if loading
             CircularProgressIndicator()
         }
 
-        is AccountViewModel.SearchState.Success -> {
+        is AccountViewModel.SearchState.Success -> {    //add next page of results to currently displayed results if search success
             val searchResult = (searchState as AccountViewModel.SearchState.Success).accountInfo
             if(!viewModel.currentResults.containsAll(searchResult)) {
                 viewModel.currentResults.addAll(searchResult)
             }
-//                    Text("User " + searchResult[0].owner.login + " found!")
-//                    Text("First repo found is called: ${searchResult[0].name}")
-            onSuccess()
-            if(searchResult.size < perPage){
+            onSuccess()     //handle display on successful search
+            if(searchResult.size < perPage){    //handle when there are no more results to display
                 onEndOfResults()
             }
         }
 
-        is AccountViewModel.SearchState.Error -> {
+        is AccountViewModel.SearchState.Error -> {  //on error, display error
             val errorMessage =
                 (searchState as AccountViewModel.SearchState.Error).errorMessage
             Text("Error: $errorMessage", fontSize = 20.sp)
@@ -86,15 +84,16 @@ fun MakeSearch(searchState: AccountViewModel.SearchState,
     }
 }
 
+//function to show search and results
 @Composable
 fun GitSearchScreen(viewModel: AccountViewModel = viewModel()) {
     var login by rememberSaveable { mutableStateOf("") }
-    var searchedLogin by rememberSaveable { mutableStateOf("") }
+    var searchedLogin by rememberSaveable { mutableStateOf("") }    //holds the name last searched for display purposes
     val searchState by viewModel.searchState.collectAsState()
-    val perPage = 10
-    var page by rememberSaveable { mutableStateOf(1)}
-    var displayResults by rememberSaveable { mutableStateOf(false) }
-    var endOfResults by rememberSaveable { mutableStateOf(false) }
+    val perPage = 10    //the number of results per page to load
+    var page by rememberSaveable { mutableStateOf(1)}   //the current page loaded. Starts at 1.
+    var displayResults by rememberSaveable { mutableStateOf(false) }    //Indicates whether to treat successful search state as new results or not.
+    var endOfResults by rememberSaveable { mutableStateOf(false) }      //indicates if there are no more results to load
     val scrollState = rememberSaveable(saver = LazyListState.Saver) { LazyListState() }
 
     Scaffold() { innerPadding ->
@@ -108,7 +107,7 @@ fun GitSearchScreen(viewModel: AccountViewModel = viewModel()) {
                 onValueChange = { login = it },
                 label = { Text("Enter Username", fontSize = 20.sp) }
             )
-            Button(onClick = {
+            Button(onClick = {      //on click, reset display flags and fetch results of current search entry
                 if (login.isNotEmpty()) {
                     displayResults = false
                     endOfResults = false
@@ -121,14 +120,14 @@ fun GitSearchScreen(viewModel: AccountViewModel = viewModel()) {
                 Text("Get Repos", fontSize = 20.sp)
             }
 
-            MakeSearch(
+            MakeSearch(     //handle current search state
                 searchState,
                 viewModel,
                 { displayResults = true },
                 { endOfResults = true },
                 perPage
             )
-            if(displayResults) {
+            if(displayResults) {    //display search results
                 DisplayResults(
                     searchedLogin,
                     viewModel,
@@ -144,6 +143,7 @@ fun GitSearchScreen(viewModel: AccountViewModel = viewModel()) {
     }
 }
 
+//function to display search results
 @Composable
 fun DisplayResults(
     login: String,
@@ -185,7 +185,7 @@ fun DisplayResults(
                 }
 
             }
-            if(!endOfResults) {
+            if(!endOfResults) {     //display a button that can fetch new entries, but only if entries still can be fetched
                 item {
                     Button(
                         onClick = {
